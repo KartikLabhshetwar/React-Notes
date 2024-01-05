29f8e84ffe10efd4285fb737da598161e784a201
KartikLabhshetwar <kartik.labhshetwar@gmail.com>
2024-01-05 16:31:21 +0530

    New folder added

---
+++ vitereactbasics/src/App.jsx
@@ -0,0 +1,10 @@
+
+function App() {
+  
+
+  return (
+      <h1>chai aur react with vite| Kartik Labhshetwar | Today I learn how to install react in your local system and run it using vite!</h1>
+  )
+}
+
+export default App
